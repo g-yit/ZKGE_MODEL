f5e10dbe4bdae726f78a39c0da3c6ed41f303b18
zhangyt <gzhangyt@outlook.com>
2026-06-24 21:54:34 +0800

    init commit add baseline code

---
+++ msrsc_baseline/code/models.py
@@ -0,0 +1,302 @@
+from abc import ABC, abstractmethod
+from typing import Tuple, List, Dict
+from torch.nn import functional as F, Parameter
+import torch
+from torch import nn
+import numpy as np
+
+
+class KBCModel(nn.Module, ABC):
+    def get_ranking(
+            self, queries: torch.Tensor,
+            filters: Dict[Tuple[int, int], List[int]],
+            batch_size: int = 1000, chunk_size: int = -1
+    ):
+        """
+        Returns filtered ranking for each queries.
+        :param queries: a torch.LongTensor of triples (lhs, rel, rhs)
+        :param filters: filters[(lhs, rel)] gives the rhs to filter from ranking
+        :param batch_size: maximum number of queries processed at once
+        :return:
+        """
+        ranks = torch.ones(len(queries))
+        with torch.no_grad():
+            b_begin = 0
+            while b_begin < len(queries):
+                these_queries = queries[b_begin:b_begin + batch_size]
+                target_idxs = these_queries[:, 2].cpu().tolist()
+                scores, _ = self.forward(these_queries)
+                targets = torch.stack([scores[row, col] for row, col in enumerate(target_idxs)]).unsqueeze(-1)
+
+                for i, query in enumerate(these_queries):
+                    filter_out = filters[(query[0].item(), query[1].item())]
+                    filter_out += [queries[b_begin + i, 2].item()]
+                    scores[i, torch.LongTensor(filter_out)] = -1e6
+                ranks[b_begin:b_begin + batch_size] += torch.sum(
+                    (scores >= targets).float(), dim=1
+                ).cpu()
+                b_begin += batch_size
+        return ranks
+
+
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=18):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x)
+        y = y.view(b, c)
+        y = self.fc(y)
+        y = y.view(b, c, 1, 1)
+        y = y.expand_as(x)
+        return x * y
+
+
+class RelationSpecificConv(torch.nn.Module):
+    def __init__(self, num_rel, in_channel, output_channel, filter_size, reshape_H, reshape_W, init_fn, emb_dim=200):
+        super(RelationSpecificConv, self).__init__()
+        self.num_rel = num_rel
+        self.in_channel = in_channel
+        self.output_channel = output_channel
+        self.h = filter_size[0]
+        self.w = filter_size[1]
+        self.dilate_height_rate = 1
+        self.dilate_width_rate = 1
+
+        if len(filter_size) == 3:
+            self.dilate_height_rate = filter_size[2]
+            self.dilate_width_rate = filter_size[2]
+        if len(filter_size) == 4:
+            self.dilate_height_rate = filter_size[2]
+            self.dilate_width_rate = filter_size[3]
+        filter_dim = self.in_channel * self.output_channel * self.h * self.w
+        if emb_dim is not None:
+            self.map = torch.nn.Linear(emb_dim, filter_dim)
+        else:
+            self.filter = torch.nn.Embedding(num_rel, filter_dim, padding_idx=0)
+        self.reshape_H, self.reshape_W = reshape_H, reshape_W
+        self.init_fn = init_fn
+        self.bn = torch.nn.BatchNorm2d(self.output_channel)
+        self.se = SELayer(self.output_channel, reduction=int(0.5 * output_channel))
+
+    def init_weights(self):
+        if hasattr(self, 'map'):
+            self.init_fn(self.map.weight)
+        else:
+            self.init_fn(self.filter.weight)
+
+    def forward(self, e1_embedded, x, rel, rel_embedded=None):
+        if rel_embedded is not None:
+            f1 = self.map(rel_embedded)
+        else:
+            f1 = self.filter(rel)
+        f1 = f1.reshape(e1_embedded.size(0) * self.in_channel * self.output_channel, 1, self.h, self.w)
+        if self.dilate_height_rate == 1 and self.dilate_width_rate == 1:
+            x = F.conv2d(x, f1, groups=e1_embedded.size(0),
+                         padding=(int((self.h - 1) // 2), int((self.w - 1) // 2)))
+        else:
+            x = F.conv2d(x, f1, groups=e1_embedded.size(0),
+                         padding=(int((self.h - 1) * self.dilate_height_rate // 2),
+                                  int((self.w - 1) * self.dilate_width_rate // 2)),
+                         dilation=(self.dilate_height_rate, self.dilate_width_rate))
+        x = x.reshape(e1_embedded.size(0), self.output_channel, self.reshape_H, self.reshape_W)
+        x = self.bn(x)
+        x = self.se(x)
+        return x
+
+
+class MSDCSE(KBCModel):
+    def __init__(self, num_ent, num_rel, embedding_dim=300, input_drop=0.4, hidden_drop=0.3, feature_map_drop=0.3,
+                 k_w=10, k_h=20, output_channel=20,
+                 filter_size_list=[(1, 5), (3, 3), (1, 9)],
+                 active_fn='relu', init_fn='xavier_normal', ce_weight=None):
+        super(MSDCSE, self).__init__()
+
+        self.embeddings = nn.ModuleList([
+            nn.Embedding(num_ent, embedding_dim),
+            nn.Embedding(num_rel, embedding_dim),
+        ])
+        self.emb_ent = self.embeddings[0]
+        self.emb_rel = self.embeddings[1]
+        self.embedding_dim = embedding_dim
+        self.num_ent = num_ent
+        self.num_rel = num_rel
+        self.perm = 1
+
+        self.k_w = k_w
+        self.k_h = k_h
+        self.ce_weight = ce_weight
+        if self.ce_weight is not None:
+            self.loss = torch.nn.CrossEntropyLoss(reduction='mean', weight=ce_weight)
+        else:
+            self.loss = torch.nn.CrossEntropyLoss()
+        self.device = torch.device('cuda')
+        self.active_fn = self.get_active_fn(active_fn)
+        self.init_fn = self.get_init_fn(init_fn)
+        self.chequer_perm = self.get_chequer_perm()
+        print("Chequer perm:", self.chequer_perm)
+        self.reshape_H = self.k_w * 2
+        self.reshape_W = self.k_h
+        self.in_channel = 1
+        self.num_filters = len(filter_size_list)
+        self.filter_size_list = filter_size_list
+        self.share = True
+
+        if isinstance(output_channel, int):
+            self.output_channels_list = [output_channel] * self.num_filters
+        else:
+            self.output_channels_list = output_channel
+
+        if len(self.output_channels_list) != self.num_filters:
+            raise ValueError("output_channels 长度必须与 filter_sizes 匹配")
+
+        self.conv_layers = torch.nn.ModuleList()
+        for i, (out_ch, filter_size) in enumerate(zip(self.output_channels_list, filter_size_list)):
+            conv = RelationSpecificConv(
+                num_rel=num_rel,
+                in_channel=self.in_channel,
+                output_channel=out_ch,
+                filter_size=filter_size,
+                reshape_H=self.reshape_H,
+                reshape_W=self.reshape_W,
+                init_fn=self.init_fn,
+                emb_dim=self.embedding_dim
+            )
+            self.conv_layers.append(conv)
+        total_channel = sum(self.output_channels_list)
+
+        self.input_drop = torch.nn.Dropout(input_drop)
+        self.hidden_drop = torch.nn.Dropout(hidden_drop)
+        self.feature_map_drop = torch.nn.Dropout2d(feature_map_drop)
+        self.bn0 = torch.nn.BatchNorm2d(self.in_channel)
+        self.bn1 = torch.nn.BatchNorm2d(total_channel)
+        self.bn2 = torch.nn.BatchNorm1d(embedding_dim)
+        fc_length = self.reshape_H * self.reshape_W * total_channel
+        self.fc = torch.nn.Linear(fc_length, embedding_dim)
+        self.register_parameter('b', Parameter(torch.zeros(num_ent)))
+
+    # ===================== 工具方法 =====================
+    def to_var(self, x, use_gpu=True):
+        if use_gpu:
+            if isinstance(x, torch.Tensor):
+                return x.long().to(self.device) if use_gpu else x.long()
+            else:
+                tensor = torch.as_tensor(x, dtype=torch.long)
+                return tensor.to(self.device) if use_gpu else tensor
+
+    def get_active_fn(self, active_fn_name):
+        fn_map = {
+            'relu': F.relu, 'leaky_relu': F.leaky_relu, 'tanh': F.tanh,
+            'sigmoid': F.sigmoid, 'silu': F.silu, 'softplus': F.softplus,
+            'gelu': F.gelu, 'elu': F.elu, 'selu': F.selu,
+        }
+        if active_fn_name not in fn_map:
+            raise ValueError("Unsupported activation function: {}".format(active_fn_name))
+        return fn_map[active_fn_name]
+
+    def get_init_fn(self, init_fn_name):
+        fn_map = {
+            'xavier_normal': torch.nn.init.xavier_normal_,
+            'xavier_uniform': torch.nn.init.xavier_uniform_,
+            'kaiming_normal': torch.nn.init.kaiming_normal_,
+            'kaiming_uniform': torch.nn.init.kaiming_uniform_,
+        }
+        return fn_map.get(init_fn_name, torch.nn.init.xavier_normal_)
+
+    def get_chequer_perm(self):
+        ent_perm = np.int32([np.random.permutation(self.embedding_dim) for _ in range(self.perm)])
+        rel_perm = np.int32([np.random.permutation(self.embedding_dim) for _ in range(self.perm)])
+        comb_idx = []
+        for k in range(self.perm):
+            temp = []
+            ent_idx, rel_idx = 0, 0
+            for i in range(self.k_h):
+                for j in range(self.k_w):
+                    if k % 2 == 0:
+                        if i % 2 == 0:
+                            temp.append(ent_perm[k, ent_idx])
+                            ent_idx += 1
+                            temp.append(rel_perm[k, rel_idx] + self.embedding_dim)
+                            rel_idx += 1
+                        else:
+                            temp.append(rel_perm[k, rel_idx] + self.embedding_dim)
+                            rel_idx += 1
+                            temp.append(ent_perm[k, ent_idx])
+                            ent_idx += 1
+                    else:
+                        if i % 2 == 0:
+                            temp.append(rel_perm[k, rel_idx] + self.embedding_dim)
+                            rel_idx += 1
+                            temp.append(ent_perm[k, ent_idx])
+                            ent_idx += 1
+                        else:
+                            temp.append(ent_perm[k, ent_idx])
+                            ent_idx += 1
+                            temp.append(rel_perm[k, rel_idx] + self.embedding_dim)
+                            rel_idx += 1
+            comb_idx.append(temp)
+        chequer_perm = torch.LongTensor(np.int32(comb_idx)).to(self.device)
+        return chequer_perm
+
+    # ===================== 初始化 =====================
+    def init(self):
+        init_fn = self.init_fn
+        init_fn(self.emb_ent.weight.data)
+        init_fn(self.emb_rel.weight.data)
+        for conv_layer in self.conv_layers:
+            conv_layer.init_weights()
+
+    # ===================== 前向传播 =====================
+    def forward(self, x):
+        """
+        前向传播。
+        """
+        e1 = x[:, 0]
+        rel = x[:, 1]
+        e2 = x[:, 2]
+
+        z, e1_embedded, rel_embedded = self._calcate_emebedding(e1, rel)
+        e2_embedded = self.emb_ent(e2)
+
+        weight = self.emb_ent.weight.transpose(1, 0)
+        pred = torch.mm(z, weight)
+        pred = pred + self.b.expand_as(pred)
+
+        return pred, [(e1_embedded, rel_embedded, e2_embedded)]
+
+    # ===================== 嵌入计算核心 =====================
+    def _calcate_emebedding(self, e1, rel):
+        e1 = self.to_var(e1)
+        rel = self.to_var(rel)
+        e1_embedded = self.emb_ent(e1)
+        rel_embedded = self.emb_rel(rel)
+        comb_emb = torch.cat([e1_embedded, rel_embedded], dim=1)
+        chequer_perm = comb_emb[:, self.chequer_perm]
+        stack_inp = chequer_perm.reshape((-1, self.perm, 2 * self.k_w, self.k_h))
+        x = self.bn0(stack_inp)
+        x = self.input_drop(x)
+        x = x.permute(1, 0, 2, 3)
+
+        outputs = []
+        for conv in self.conv_layers:
+            output = conv(e1_embedded, x, rel, rel_embedded)
+            outputs.append(output)
+
+        x = torch.cat(outputs, dim=1)
+        x = self.active_fn(x)
+        x = self.feature_map_drop(x)
+
+        x = x.view(x.shape[0], -1)
+        x = self.fc(x)
+        x = self.hidden_drop(x)
+        x = self.bn2(x)
+        x = self.active_fn(x)
+        return x, e1_embedded, rel_embedded
---
+++ msrsc_baseline/code/optimizers.py
@@ -0,0 +1,64 @@
+import tqdm
+import torch
+import numpy as np
+from torch import nn
+from torch import optim
+from models import KBCModel
+
+
+class KBCOptimizer(object):
+    def __init__(
+            self, dsl, ds, model_name, model: KBCModel, regularizer: list, optimizer: optim.Optimizer,
+            batch_size: int = 256, temp: float = 1.0, rank: int = 2000, out_size: int = 2000,
+            verbose: bool = True, scheduler=None,
+    ):
+        self.dsl = dsl
+        self.ds = ds
+        self.model_name = model_name
+        self.scheduler = scheduler
+        print(f"Scheduler: {self.scheduler}")
+        self.model = model
+        self.regularizer = regularizer[0]
+        self.regularizer2 = regularizer[1]
+        self.optimizer = optimizer
+        self.batch_size = batch_size
+        self.verbose = verbose
+        self.temperature = temp
+        self.rank = rank
+        self.out_size = out_size
+
+    def epoch(self, examples: torch.LongTensor, e=0, weight=None):
+        self.model.train()
+        actual_examples = examples[torch.randperm(examples.shape[0]), :]
+        loss = nn.CrossEntropyLoss(reduction='mean', weight=weight)
+        loss_fit_list = []
+
+        with tqdm.tqdm(total=examples.shape[0], unit='ex', disable=not self.verbose) as bar:
+            bar.set_description(f'train loss')
+            b_begin = 0
+            while b_begin < examples.shape[0]:
+                input_batch = actual_examples[
+                              b_begin:b_begin + self.batch_size
+                              ].cuda()
+                truth = input_batch[:, 2]
+
+                predictions, factors = self.model.forward(input_batch)
+
+                l_fit = loss(predictions, truth)
+                l_reg = self.regularizer.forward(factors)
+
+                l = l_fit + l_reg
+
+                loss_fit_list.append(l_fit.detach().cpu().numpy())
+                self.optimizer.zero_grad()
+                l.backward()
+                self.optimizer.step()
+                b_begin += self.batch_size
+                bar.update(input_batch.shape[0])
+
+                postfix = {'loss': f'{l.item():.2f}'}
+                bar.set_postfix(**postfix)
+
+        if self.scheduler is not None:
+            self.scheduler.step(np.average(loss_fit_list))
+        return l
